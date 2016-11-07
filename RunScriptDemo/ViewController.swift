59af0764f6d42302e5bf6433f063054b92c811c8
Giovanni Lodi <giovanni.lodi42@gmail.com>
2016-11-07 14:06:23 +1100

    First commit

---
+++ RunScriptDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RunScriptDemo
+//
+//  Created by Giovanni on 7/11/16.
+//  Copyright © 2016 mokacoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        _ = URL(string: "http://test.com")!
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
